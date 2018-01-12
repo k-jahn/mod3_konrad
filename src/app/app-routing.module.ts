0a64483e7f6916912f1308c63671c7af4b1791cd
konrad <konrad.jahn.84@gmail.com>
2018-01-12 13:39:32 +0100

    circular redesign, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 // components
+import { MainComponent } from './component/main.component';
+import { GameDetailComponent } from './component/game-detail.component';
+import { TeamDetailComponent } from './component/team-detail.component';
 import { HomeComponent } from './component/home.component';
 import { GamesComponent } from './component/games.component';
 import { TeamsComponent } from './component/teams.component';
 
 // route definitions ----------------------------------------
 const routes: Routes = [
-  { path: '', redirectTo: '/games', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'games', component: GamesComponent },
-  { path: 'teams', component: TeamsComponent }
+  { path: '', redirectTo: '/main/games', pathMatch: 'full' },
+  { path: 'main', component: MainComponent, children: [
+    { path: 'home', component: HomeComponent },
+    { path: 'games', component: GamesComponent },
+    { path: 'teams', component: TeamsComponent },
+  ]},
+  { path: 'game/:id', component: GameDetailComponent },
+  { path: 'team/:id', component: TeamDetailComponent },
 ];
 // ------------------------------------------------------------------
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { environment } from './../environments/environment';
 
-// Dependencies =====================================================
+// Stock Dependencies =====================================================
 // ng
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -12,35 +12,40 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 
-
-// App =========================================================================
+// App Dependencies =========================================================================
 // components
 import { AppComponent } from './component/app.component';
-import { HomeComponent } from './component/home.component';
+import { GameDetailComponent } from './component/game-detail.component';
 import { GamesComponent } from './component/games.component';
+import { HomeComponent } from './component/home.component';
+import { MainComponent } from './component/main.component';
+import { TeamDetailComponent } from './component/team-detail.component';
 import { TeamsComponent } from './component/teams.component';
 // services
 import { EventService } from './service/event.service';
 import { GameService } from './service/game.service';
 import { TeamService } from './service/team.service';
+// modules
 import { AppRoutingModule } from './app-routing.module';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    GameDetailComponent,
     GamesComponent,
-    TeamsComponent
+    HomeComponent,
+    MainComponent,
+    TeamDetailComponent,
+    TeamsComponent,
   ],
   imports: [
     AngularFireAuthModule,
     AngularFireDatabaseModule,
     AngularFireModule.initializeApp(environment.firebase),
+    AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
     MatTabsModule,
-    AppRoutingModule,
   ],
   providers: [
     EventService,
